6e5230823025b75f1c8db415b787dcf9a9eae085
IskandarKhanbekov <khanbekovin@gmail.com>
2022-11-01 17:16:06 +0100

    version 1.0.2 -better looking messages in slack -some fixes

--- slack.py
+++ slack.py
@@ -5,13 +5,13 @@ import json
 def send_message_to_slack_channel(
     url, text, author_name, title, color):
     data = {
-        "text": text,
         "attachments": [
             {
-                "author_name": author_name,
-                "text": text,
                 "color": color,
-                "fields": [{"title": title, "short": "false"}],
+                "author_name": author_name,
+                "title": title,
+                "text": text,
+                "fields": [{"short": "false"}],
             },
         ],
     }
