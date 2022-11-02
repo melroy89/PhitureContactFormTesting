baebe2ec57adc6cf78d53e712f01c25c1c445739
IskandarKhanbekov <khanbekovin@gmail.com>
2022-11-02 20:22:12 +0100

    version 1.0.3 -positive and negative scenarios (with colors) -better stability -useful info in Slack messages

--- slack.py
+++ slack.py
@@ -2,15 +2,33 @@ import requests
 import json
 
 
-def send_message_to_slack_channel(
-    url, text, author_name, title, color):
+def send_positive_message_to_slack_channel(
+    url, text, title, color):
     data = {
         "attachments": [
             {
-                "color": color,
-                "author_name": author_name,
-                "title": title,
                 "text": text,
+                "title": title,
+                "color": color,
+                "fields": [{"short": "false"}],
+            },
+        ],
+    }
+    headers = {"Content-type": "application/json"}
+    if url is not None:
+        # skip when short url
+        if len(url) > 15:
+            requests.post(url, data=json.dumps(data), headers=headers)
+
+
+def send_negative_message_to_slack_channel(
+    url, text, title, color):
+    data = {
+        "attachments": [
+            {
+                "text": text,
+                "title": title,
+                "color": color,
                 "fields": [{"short": "false"}],
             },
         ],
