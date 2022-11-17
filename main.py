24a1bd55ed68784c7937a429503a3c8370561016
Thylacine <53875338+BadThylacine@users.noreply.github.com>
2022-11-17 16:17:01 +0100

    Experiment with elif

--- main.py
+++ main.py
@@ -175,7 +175,7 @@ def sendContactForm():
             element = driver.find_element(By.XPATH, '//div[contains(text(), "successfully")]').is_displayed()
             if element:
                 element = True
-            elif i == 9 and not element:
+            elif i == 1 and not element:
                 print("Trying to refresh page and fill everything again")
                 driver.refresh()
                 findAndFillOutAllTextFields()
